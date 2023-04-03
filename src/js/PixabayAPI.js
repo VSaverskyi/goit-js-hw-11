f3b221f1832d875c0985c5ca054d08cd45a69cf8
VSaverskyi <vovikck15@gmail.com>
2023-04-03 18:36:13 +0200

    remove try/catch from PixabayAPI

--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -4,18 +4,12 @@ export class PixabayAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '34862822-8eb02c3fef422917f168c53cc';
 
-  query = null;
-  page = 1;
-  count = 40;
+  constructor() {
+    this.query = null;
+    this.page = 1;
+    this.count = 40;
+  }
 
-  // basedSearchParams = {
-  //   key: this.#API_KEY,
-  //   image_type: 'photo',
-  //   orientation: 'horizontal',
-  //   safesearch: 'true',
-  //   page: this.page,
-  //   per_page: this.count,
-  // };
   async fetchPhotos() {
     const searchParams = new URLSearchParams({
       q: this.query,
@@ -26,12 +20,9 @@ export class PixabayAPI {
       page: this.page,
       per_page: this.count,
     });
-    try {
-      return await axios.get(`${this.#BASE_URL}`, {
-        params: searchParams,
-      });
-    } catch (err) {
-      throw new Error(err.message);
-    }
+
+    return await axios.get(`${this.#BASE_URL}`, {
+      params: searchParams,
+    });
   }
 }
